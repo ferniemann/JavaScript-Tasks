e50df7480e3d7ad5c0f0dcc1fff42fb898c505e4
ferniemann <ferdinand.niemann@gmail.com>
2021-03-07 22:47:34 +0100

    Level 1 loop

--- Lev1/loops/1_9loopsinput/main.js
+++ Lev1/loops/1_9loopsinput/main.js
@@ -1,8 +1,6 @@
 let words = ['hallo', 'Auto', 'Waschmaschine', 'Schrank', 'Katze', 'Beispiel', 'Eltern', 'Fenster', 'Geburtstag', 'Himmel', 'schwimmen', 'Zeitung']
 let number = document.getElementById('number')
 let print = document.getElementById('print')
-console.log(words.length)
-console.log(words[1].length)
 
 printWord = () => {
     for (let i = 0; i < words.length; i++) {
